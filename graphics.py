94d607282bfd6c5540a6483b50848516c755e8dd
diordna <diordna@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-08-08 00:05:06 +0000

    Possible fix for Linux bug
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@27 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- graphics.py
+++ graphics.py
@@ -1,4 +1,4 @@
-import math
+import math, sys, settings
 import pyglet.graphics, pyglet.image, pyglet.gl
 
 _canvas_pre = None
@@ -54,4 +54,10 @@ def draw_quad(*args):
 	pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', args))
 
 def get_snapshot():
+	if sys.platform == 'Linux':
+		return pyglet.image.get_buffer_manager().get_color_buffer().get_region(
+			settings.settings['window_width']-settings['canvas_width'],
+			settings.settings['window_height']-settings['canvas_height'],
+			settings['canvas_width'], settings['canvas_height']
+		)
 	return pyglet.image.get_buffer_manager().get_color_buffer().image_data
