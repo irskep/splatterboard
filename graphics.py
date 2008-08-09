9b524de16ac9b5855515f0266c59d9704e8f100f
jleedev <jleedev@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-08-09 04:07:20 +0000

    Decorators!!
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@43 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- graphics.py
+++ graphics.py
@@ -28,7 +28,6 @@ def triplecall_wrapper(func):
 		function_stack_2.append((func, args, kwargs))
 	return new_func
 
-
 def get_snapshot():
 	return pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
 
@@ -41,43 +40,42 @@ def get_pixel_from_image(image, x, y):
 	b = data[2]
 	return (float(r)/255.0,float(g)/255.0,float(b)/255.0,1.0)
 
-
 def call_twice(func, *args, **kwargs):
 	func(*args,**kwargs)
 	function_stack.append((func,args,kwargs))
 
-def _set_color(r=0.0, g=0.0, b=0.0, a=1.0, color=None):
+@doublecall_wrapper
+def set_color(r=0.0, g=0.0, b=0.0, a=1.0, color=None):
 	if color is not None: pyglet.gl.glColor4f(*color)
 	else: pyglet.gl.glColor4f(r,g,b,a)
-set_color = doublecall_wrapper(_set_color)
 
-def _clear(r=0.0, g=0.0, b=0.0, a=1.0, color=None):	
+@doublecall_wrapper
+def clear(r=0.0, g=0.0, b=0.0, a=1.0, color=None):	
 	if color is not None: pyglet.gl.glClearColor(*color)
 	else: pyglet.gl.glClearColor(1,1,1,1);
 	for window in pyglet.app.windows.__iter__():
 		window.clear()
-clear = triplecall_wrapper(_clear)
 
-def _draw_image(img, x, y):
+@doublecall_wrapper
+def draw_image(img, x, y):
 	img.blit(x,y)
-draw_image = doublecall_wrapper(_draw_image)
 
-def _draw_label(label):
+@doublecall_wrapper
+def draw_label(label):
 	label.draw()
-draw_label = doublecall_wrapper(_draw_label)	
 
-def _draw_line(x1, y1, x2, y2):
+@doublecall_wrapper
+def draw_line(x1, y1, x2, y2):
 	pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2f', (x1, y1, x2, y2)))
-draw_line = doublecall_wrapper(_draw_line)
 
-def _draw_rect(x1, y1, x2, y2):	
+@doublecall_wrapper
+def draw_rect(x1, y1, x2, y2):	
 	pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', (x1, y1, x1, y2, x2, y2, x2, y1)))
-draw_rect = doublecall_wrapper(_draw_rect)
 
-def _draw_rect_outline(x1, y1, x2, y2):
+@doublecall_wrapper
+def draw_rect_outline(x1, y1, x2, y2):
 	pyglet.graphics.draw(5, pyglet.gl.GL_LINE_STRIP, 
 		('v2f', (x1, y1, x1, y2, x2, y2, x2, y1, x1, y1)))
-draw_rect_outline = doublecall_wrapper(_draw_rect_outline)
 
 def concat(it):
 	return (y for x in it for y in x)
@@ -103,17 +101,17 @@ def iter_ellipse(x1, y1, x2, y2):
 		yield (x + math.cos(a) * xrad, y + math.sin(a) * yrad)
 		a += da
 
-def _draw_ellipse(x1, y1, x2, y2):
+@doublecall_wrapper
+def draw_ellipse(x1, y1, x2, y2):
 	points = list(concat(iter_ellipse(x1, y1, x2, y2)))
 	pyglet.graphics.draw(len(points)/2, pyglet.gl.GL_TRIANGLE_FAN, ('v2f', points))
-draw_ellipse = doublecall_wrapper(_draw_ellipse)
 
-def _draw_ellipse_outline(x1, y1, x2, y2):
+@doublecall_wrapper
+def draw_ellipse_outline(x1, y1, x2, y2):
 	points = list(concat(iter_ellipse(x1, y1, x2, y2)))
 	points.extend(points[:2])
 	pyglet.graphics.draw(len(points)/2, pyglet.gl.GL_LINE_STRIP, ('v2f', points))
-draw_ellipse_outline = doublecall_wrapper(_draw_ellipse_outline)
 
-def _draw_quad(*args):
+@doublecall_wrapper
+def draw_quad(*args):
 	pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', args))
-draw_quad = doublecall_wrapper(_draw_quad)
