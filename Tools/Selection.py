a32b8e09c5c6d6aa9f94bca800ac9ae96098d642
diordna <diordna@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-08-10 21:19:54 +0000

    Selection tool be coming progressively more useful
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@65 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- Tools/Selection.py
+++ Tools/Selection.py
@@ -4,7 +4,8 @@ class Selection(SplatboardTool.Tool):
 	"""Simple rect tool"""
 	
 	canvas_pre = None
-	sx, sy, w, h = 0.0, 0.0, 0.0, 0.0
+	x1, y1, x2, y2 = 0.0, 0.0, 0.0, 0.0
+	w, h = 0.0, 0.0
 	mouse_offset_x, mouse_offset_y = 0.0, 0.0
 	dragging = False
 	
@@ -13,26 +14,30 @@ class Selection(SplatboardTool.Tool):
 	
 	def start_drawing(self, x, y):
 		if self.coords_in_selection(x,y):
-			self.mouse_offset_x = x - self.sx
-			self.mouse_offset_y = y - self.sy
+			print 'dragging'
+			self.mouse_offset_x = x - self.x1
+			self.mouse_offset_y = y - self.y1
 			self.dragging = True
 		else:
-			self.sx, self.sy = x, y
+			self.x1, self.y1 = x, y
 			self.dragging = False
 	
 	def keep_drawing(self, x, y, dx, dy):
 		if self.dragging:
-			self.sx = x - self.mouse_offset_x
-			self.sy = y - self.mouse_offset_y
-		else:
-			graphics.enable_line_stipple()
-			self.w, self.h = x - self.sx, y - self.sy
-			graphics.set_color(1,1,1,1)
-			graphics.draw_image(self.canvas_pre,0,0)
-			graphics.set_line_width(1.0)
-			graphics.set_color(color=graphics.line_color)
-			graphics.draw_rect_outline(self.sx, self.sy, self.sx + self.w, self.sy + self.h)
-			graphics.disable_line_stipple()
+			self.x1 = x - self.mouse_offset_x
+			self.y1 = y - self.mouse_offset_y
+			self.x2, self.y2 = self.x1 + self.w, self.y1 + self.h
+		else:	
+			self.x2, self.y2 = x, y
+			self.w = self.x2 - self.x1
+			self.h = self.y2 - self.y1
+		graphics.enable_line_stipple()
+		graphics.set_color(1,1,1,1)
+		graphics.draw_image(self.canvas_pre,0,0)
+		graphics.set_line_width(1.0)
+		graphics.set_color(color=graphics.line_color)
+		graphics.draw_rect_outline(self.x1, self.y1, self.x2, self.y2)
+		graphics.disable_line_stipple()
 	
 	def stop_drawing(self, x, y):
 		#self.keep_drawing(x, y, 0, 0)
@@ -43,7 +48,10 @@ class Selection(SplatboardTool.Tool):
 		self.dragging = False
 	
 	def coords_in_selection(self, x, y):
-		return x >= self.sx and y >= self.sy and x <= self.sx + self.w and y <= self.sy + self.h
+		x1, y1, x2, y2 = self.x1, self.y1, self.x2, self.y2
+		if x2 < x1: x1, x2 = x2, x1
+		if y2 < y1: y1, y2 = y2, y1
+		return x > x1 and y > y1 and x < x2 and y < y2
 
 default = Selection()
 priority = 88
