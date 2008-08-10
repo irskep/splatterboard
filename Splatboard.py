b856c391d3cc4be5858650ea9a977c42b5d57c02
diordna <diordna@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-08-10 03:25:24 +0000

    Merged loader.py into Splatboard.py
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@48 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- Splatboard.py
+++ Splatboard.py
@@ -158,6 +158,19 @@ class Splatboard(pyglet.window.Window):
 		pyglet.app.exit()
 	
 	#------------TOOL THINGS------------#
+	def import_libs(dir):
+		""" Imports the libs, returns a dictionary of the libraries."""
+		library_dict = {}
+		sys.path.append(dir)
+		for f in os.listdir(os.path.abspath(dir)):
+			module_name, ext = os.path.splitext(f)
+			if ext == '.py' and module_name != '__init__':
+				print 'imported module: %s' % (module_name)
+				module = __import__(module_name)
+				library_dict[module_name] = module
+
+		return library_dict
+	
 	def load_tools(self):
 		#Import everything in the Tools directory, shove them in a dictionary
 		self.tools = loader.import_libs('Tools')
