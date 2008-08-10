ce4fcc97762fe927b57e8a3a70a6261923501d38
jleedev <jleedev@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-08-10 16:20:01 +0000

    My ellipses are hawt now.
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@54 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- graphics.py
+++ graphics.py
@@ -93,21 +93,28 @@ def draw_rect_outline(x1, y1, x2, y2):
 def concat(it):
 	return list(y for x in it for y in x)
 
-def iter_ellipse(x1, y1, x2, y2):
+def iter_ellipse(x1, y1, x2, y2, da=None, step=None):
 	xrad = abs((x2-x1) / 2.0)
 	yrad = abs((y2-y1) / 2.0)
 	x = (x1+x2) / 2.0
 	y = (y1+y2) / 2.0
 	
-	# use the average of the radii to compute the angle step
-	# shoot for segments that are 8 pixels long
-	step = 8.0
-	rad = max((xrad+yrad)/2, 0.01)
-	rad_ = max(min(step / rad / 2.0, 1), -1)
-	
-	# but if the circle is too small, that would be ridiculous
-	# use pi/16 instead.
-	da = min(2 * math.asin(rad_), math.pi / 16)
+	if da and step:
+		raise ValueError("Can only set one of da and step")
+
+	if not da and not step:
+		step = 8.0
+
+	if not da:
+		# use the average of the radii to compute the angle step
+		# shoot for segments that are 8 pixels long
+		step = 32.0
+		rad = max((xrad+yrad)/2, 0.01)
+		rad_ = max(min(step / rad / 2.0, 1), -1)
+		
+		# but if the circle is too small, that would be ridiculous
+		# use pi/16 instead.
+		da = min(2 * math.asin(rad_), math.pi / 16)
 	
 	a = 0.0
 	while a <= math.pi * 2:
@@ -121,8 +128,35 @@ def draw_ellipse(x1, y1, x2, y2):
 
 @doublecall_wrapper
 def draw_ellipse_outline(x1, y1, x2, y2):
-	points = concat(iter_ellipse(x1, y1, x2, y2))
-	pyglet.graphics.draw(len(points)/2, pyglet.gl.GL_LINE_LOOP, ('v2f', points))
+	w2 = selections.line_size / 2.0
+	x_dir = 1 if x2 > x1 else -1
+	y_dir = 1 if y2 > y1 else -1
+
+	x1_out = x1 - x_dir * w2
+	x1_in = x1 + x_dir * w2
+	x2_out = x2 + x_dir * w2
+	x2_in = x2 - x_dir * w2
+
+	y1_out = y1 - y_dir * w2
+	y1_in = y1 + y_dir * w2
+	y2_out = y2 + y_dir * w2
+	y2_in = y2 - y_dir * w2
+
+	points_inner = list(iter_ellipse(x1_in, y1_in, x2_in, y2_in, da=0.1))
+	points_outer = list(iter_ellipse(x1_out, y1_out, x2_out, y2_out, da=0.1))
+
+	points_stroke = concat(concat(zip(points_inner, points_outer)))
+	points_stroke.extend(points_stroke[:4]) # draw the first *two* points again
+	points_inner = concat(points_inner)
+	points_outer = concat(points_outer)
+
+	pyglet.gl.glLineWidth(1)
+	pyglet.graphics.draw(len(points_stroke)/2,
+			pyglet.gl.GL_TRIANGLE_STRIP, ('v2f', points_stroke))
+	pyglet.graphics.draw(len(points_inner)/2,
+			pyglet.gl.GL_LINE_LOOP, ('v2f', points_inner))
+	pyglet.graphics.draw(len(points_outer)/2,
+			pyglet.gl.GL_LINE_LOOP, ('v2f', points_outer))
 
 @doublecall_wrapper
 def draw_quad(*args):
