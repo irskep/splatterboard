21782b4216aecfddcb84ece3d6ff9bc589686f85
diordna <diordna@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-11-22 20:23:14 +0000

    Polygon tool finalized. Still needs icon.
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@158 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- Tools/Polygon.py
+++ Tools/Polygon.py
@@ -1,4 +1,5 @@
-import tool, resources, graphics, pyglet, math
+import pyglet, math
+import tool, resources, graphics, gui
 
 class Polygon(tool.Tool):
     """Simple polygon tool"""
@@ -6,10 +7,38 @@ class Polygon(tool.Tool):
     canvas_pre = None
     x, y, rx, ry = 0.0, 0.0, 0.0, 0.0
     sides = 3
+    num_buttons = 6
     
     def select(self):
         self.canvas_pre = graphics.get_canvas()
         tool.generate_line_selector()
+        
+        self.button_group = gui.ButtonGroup()
+        buttons = []
+        
+        def generate_side_function(n):
+            def temporary_function():
+                self.sides = n
+            return temporary_function
+        
+        def generate_poly_button(x, y, w, h, n):
+            def poly_func():
+                graphics.set_color(1,1,1,1)
+                graphics.draw_polygon(self.generate_polygon(x+w/2,y+h/2,x+w/2,y+h-5,n))
+                graphics.set_color(0,0,0,1)
+                graphics.set_line_width(1)
+                graphics.draw_polygon_outline(self.generate_polygon(x+w/2,y+h/2,x+w/2,y+h-3,n))
+            return poly_func
+        
+        w, h = resources.SquareButton.width, resources.SquareButton.height
+        for i in xrange(0,self.num_buttons):
+            temporary_button = gui.Button(resources.SquareButton, generate_side_function(i+3),
+                                            i*w+5, h+5, "", parent_group=self.button_group,
+                                            more_draw = generate_poly_button(i*w+5, h+5, w, h, i+3))
+            buttons.append(temporary_button)
+            tool.controlspace.add(temporary_button)
+            buttons[0].select()
+            buttons[0].action()
     
     def start_drawing(self, x, y):
         self.x, self.y = x, y
@@ -20,21 +49,21 @@ class Polygon(tool.Tool):
         graphics.draw_image(self.canvas_pre,graphics.canvas_x,graphics.canvas_y)
         graphics.set_line_width(graphics.line_size)
         graphics.set_color(color=graphics.fill_color)
-        graphics.draw_polygon(self.generate_polygon(self.x,self.y,self.rx,self.ry));
+        graphics.draw_polygon(self.generate_polygon(self.x,self.y,self.rx,self.ry,self.sides));
         graphics.set_color(color=graphics.line_color)
-        graphics.draw_polygon_outline(self.generate_polygon(self.x,self.y,self.rx,self.ry));
-        graphics.draw_points(self.generate_polygon(self.x,self.y,self.rx,self.ry));
+        graphics.draw_polygon_outline(self.generate_polygon(self.x,self.y,self.rx,self.ry,self.sides));
+        graphics.draw_points(self.generate_polygon(self.x,self.y,self.rx,self.ry,self.sides));
     
     def stop_drawing(self, x, y):
         self.keep_drawing(x, y, 0, 0)
         self.canvas_pre = graphics.get_canvas()
     
-    def generate_polygon(self, x, y, rx, ry):
+    def generate_polygon(self, x, y, rx, ry, n):
         radius = math.sqrt((rx - x)*(rx - x)+(ry - y)*(ry - y))
         theta = math.atan2(ry - y, rx - x)
         li = []
-        for i in xrange(3):
-            theta += 2 * math.pi / self.sides
+        for i in xrange(n):
+            theta += 2 * math.pi / n
             li.extend([radius * math.cos(theta) + x, radius * math.sin(theta) + y])
         return li
 
