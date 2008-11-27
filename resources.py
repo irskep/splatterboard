805ff62c957bbcbc632f8171ba855eed5c8a66c4
jleedev <jleedev@b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7>
2008-11-27 05:29:30 +0000

    retab.
    git-svn-id: http://cwru-hackers.googlecode.com/svn/splatterboard/trunk@213 b4ba7dc6-1a4e-0410-89e8-6bac2d59bbf7

--- resources.py
+++ resources.py
@@ -10,13 +10,13 @@ Load resources with minimal effort.
     4. Import the module.
     
     5. Refer to images and sounds as resources.your_resource. Automatically updates
-    	when you add new resources to your folder.
+        when you add new resources to your folder.
 """
 
 import pyglet, os
 
-supported_image_formats = [	'bmp','dds','exif','gif','jpg','jpeg','jp2','jpx',
-							'pcx','png','pnm','ras','tga','tif','tiff', 'xbm', 'xpm']
+supported_image_formats = [ 'bmp','dds','exif','gif','jpg','jpeg','jp2','jpx',
+                            'pcx','png','pnm','ras','tga','tif','tiff', 'xbm', 'xpm']
 
 #Change this to fit your folder structure
 resource_paths = ['Tools','Resources','Tool Resources']
@@ -26,11 +26,11 @@ exclude = []
 loaded = False
 
 function_pairs = {
-	#'ext':(func, {args})
-	#I only included the most common formats. Copy/paste to do more.
-	'mp3':(pyglet.resource.media,{'streaming':False}),
-	'ogg':(pyglet.resource.media,{'streaming':False}),
-	'wav':(pyglet.resource.media,{'streaming':False})
+    #'ext':(func, {args})
+    #I only included the most common formats. Copy/paste to do more.
+    'mp3':(pyglet.resource.media,{'streaming':False}),
+    'ogg':(pyglet.resource.media,{'streaming':False}),
+    'wav':(pyglet.resource.media,{'streaming':False})
 }
 
 def load():
@@ -43,9 +43,9 @@ def load():
 
     #Then a miracle occurs!
     for path in pyglet.resource.path:
-    	for file_name in os.listdir(path):
-    		name, ext = os.path.splitext(file_name)
-    		if name not in exclude:
-    			for key, (func, kwargs) in function_pairs.iteritems():
-    				if ext == '.'+key and os.path.exists(path):
-    					globals()[name] = func(file_name,**kwargs)
+        for file_name in os.listdir(path):
+            name, ext = os.path.splitext(file_name)
+            if name not in exclude:
+                for key, (func, kwargs) in function_pairs.iteritems():
+                    if ext == '.'+key and os.path.exists(path):
+                        globals()[name] = func(file_name,**kwargs)
